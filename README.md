94335dd5ea97e74765b02533a907ace7c5c0d5e5
Lucas <lucas.pedroso@ambevtech.com.br>
2024-10-14 17:07:07 -0300

    FEAT: Add setup de PC

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # lens
+
+### Para executar num PC ou MiniPC
+```bash
+/bin/bash -c "$(curl -fsSL raw.githubusercontent.com/sohdahvisao/lens/main/setup_pc.sh)"
+
+```
+
+### Para executar num raspberry:
 ```bash 
-/bin/bash -c "$(curl -fsSL raw.githubusercontent.com/sohdahvisao/lens/main/setup.sh)"
+/bin/bash -c "$(curl -fsSL raw.githubusercontent.com/sohdahvisao/lens/main/setup_rasp.sh)"
 ```
---
+++ setup_pc.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -ex
+
+echo "[INFO] Instalando dependencias.."
+sudo apt update
+sudo apt install git
+sudo apt install python-is-python3
+alias python=python3
+sudo apt install python3-venv
+
+echo "[INFO] Instalando arquivos de script.."
+git clone https://AMBEV-SA@dev.azure.com/AMBEV-SA/ambevtech-brewtech-soda-vision/_git/edge-device ~/sodavision
+cd ~/sodavision/
+
+echo "[INFO] Adicionando permissões aos scripts.."
+cd ~/sodavision/sodalens/
+sudo chmod +x ~/sodavision/sodalens/*.sh
+
+echo "[INFO] Python Ambiente Virtual"
+python -m venv ~/sodavision/sodalens/.venv
+source ~/sodavision/sodalens/.venv/bin/activate
+pip install -r requirements.txt
+
+echo "[INFO] Criando Atalhos"
+if [ ! -e ~/Desktop/capturador ]; then
+  ln -sf ~/sodavision/sodalens/capture.sh ~/Desktop/capturador
+fi
+
+if [ ! -e ~/Desktop/zipper ]; then
+  ln -sf ~/sodavision/sodalens/zipper.sh ~/Desktop/zipper
+fi
+
+if [ ! -d ~/Desktop/datasets ]; then
+  ln -sf ~/sodavision/sodalens/datasets ~/Desktop/datasets
+fi
+
+if [ ! -d ~/Desktop/modelos ]; then
+  ln -sf ~/sodavision/sodalens/modelos ~/Desktop/modelos
+fi
+
+if [ ! -d ~/Desktop/update ]; then
+  ln -sf ~/sodavision/sodalens/update-vision.sh ~/Desktop/update
+fi
+
+echo "[INFO] Instalando Docker"
+~/sodavision/sodalens/docker-installer.sh
+
+echo "[INFO] Montando as imagens Docker"
+cd ~/sodavision
+make build
+
+sudo shutdown -r now
