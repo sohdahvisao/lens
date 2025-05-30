da1e2bd00b764d5c7ed21ff03f402b0067a7814b
vnunciatelli <nunciatelli54@gmail.com>
2025-05-30 17:50:55 -0300

    feat: remmover sodalens  / criador de datasets

--- setup_pc.sh
+++ setup_pc.sh
@@ -19,40 +19,11 @@ echo "[INFO] Instalando arquivos de script.."
 git clone https://AMBEV-SA@dev.azure.com/AMBEV-SA/ambevtech-brewtech-soda-vision/_git/edge-device ~/sodavision
 cd ~/sodavision/
 
-git checkout test-arquitetura-minipc
+git checkout feat-arquitetura-minipc
 
 echo "[INFO] Adicionando permissões aos scripts.."
 cd ~/sodavision/sodalens/
 sudo chmod +x ~/sodavision/sodalens/*.sh
-
-echo "[INFO] Python Ambiente Virtual"
-python -m venv ~/sodavision/sodalens/.venv
-source ~/sodavision/sodalens/.venv/bin/activate
-pip install -r requirements.txt
-
-echo "[INFO] Criando diretorios"
-[ -d "$HOME/Desktop" ] || mkdir "$HOME/Desktop"
-
-echo "[INFO] Criando Atalhos"
-if [ ! -e ~/Desktop/capturador ]; then
-  ln -sf ~/sodavision/sodalens/capture.sh ~/Desktop/capturador
-fi
-
-if [ ! -e ~/Desktop/zipper ]; then
-  ln -sf ~/sodavision/sodalens/zipper.sh ~/Desktop/zipper
-fi
-
-if [ ! -d ~/Desktop/datasets ]; then
-  ln -sf ~/sodavision/sodalens/datasets ~/Desktop/datasets
-fi
-
-if [ ! -d ~/Desktop/modelos ]; then
-  ln -sf ~/sodavision/sodalens/modelos ~/Desktop/modelos
-fi
-
-if [ ! -d ~/Desktop/update ]; then
-  ln -sf ~/sodavision/sodalens/update-vision.sh ~/Desktop/update
-fi
 
 echo "[INFO] Instalando Docker"
 ~/sodavision/sodalens/docker-installer.sh
